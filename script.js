0aabaef00d0ec73767939fa78991aa3df3fd344e
Sathwik puttala <sathwikputtala2003@gmail.com>
2025-12-31 16:54:08 +0530

    portfolio files added

---
+++ script.js
@@ -0,0 +1,9 @@
+let pic= document.querySelector(".pic")
+let link= document.querySelector(".nav-links")
+
+function changeIcon(){
+ pic.classList.toggle("fa-bars")
+ pic.classList.toggle("fa-xmark")
+ link.classList.toggle("active")
+
+}  
